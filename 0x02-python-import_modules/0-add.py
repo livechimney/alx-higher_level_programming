410add91a45c2c375bf30dcb0feb9d0528b46774
livechimney <adrianmuriuki1366@gmail.com>
2023-11-30 10:39:58 -0800

    update on import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import add_0.py
-def add_sum(a, b):
-    
+if __name__ == '__main__':
+    from add_0 import add
     a = 1
     b = 2
-  print("{} + {} = {}".format(a, b, add(a, b)))
-return (sum)
+    print("{} + {} = {}".format(a, b, add(a, b)))
