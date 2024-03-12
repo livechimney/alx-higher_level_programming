a72261e0c2a979afdf01db9bffd1427e119a2450
livechimney <adrianmuriuki1366@gmail.com>
2024-03-12 07:49:18 +0300

    java sprint 4

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,3 +4,4 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
 } else {
   console.log('My number:', parseInt(process.argv[2]));
 }
+
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -9,3 +9,4 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
     i++;
   }
 }
+
